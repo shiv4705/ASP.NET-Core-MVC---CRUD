a9224a188cd252634ccc496893a29b2e436a25ec
shiv4705 <shivpatel4705.sp@gmail.com>
2024-10-11 21:57:32 +0530

    Initial Commit'

---
+++ ProductManagement/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Product name must be between 3 and 100 characters.")]
+        public string Name { get; set; }
+
+        [Range(1, 10000, ErrorMessage = "Price must be between 1 and 10,000.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        public string Category { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description can't be longer than 200 characters.")]
+        public string Description { get; set; }
+    }
+}
